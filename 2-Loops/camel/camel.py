72e1153fa26fcbe52784b5bf43f764bc55930b4c
DashJohnson <158523793+DashJohnson@users.noreply.github.com>
2024-02-15 23:13:58 +0000

    upload completed camel.py

--- 2-Loops/camel/camel.py
+++ 2-Loops/camel/camel.py
@@ -1,3 +1,16 @@
+def camel_to_snake(name):
+    result = ""
+    for char in name:
+        result += "_" + char.lower()
+    else:
+        result += char
+    return result
+
+
 def main():
-    camel_case = input("camelCase: ").strip()
-    print("snake_case")
+    camel_case_name = input("Enter a variable name in camel case: ")
+    snake_case_name = camel_to_snake(camel_case_name)
+    print("Snake case name:", snake_case_name)
+
+
+main()
