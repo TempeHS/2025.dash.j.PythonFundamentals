b8ff958386f0d6590fc6d313b229ed60b9432e8e
DashJohnson <158523793+DashJohnson@users.noreply.github.com>
2024-02-21 11:36:56 +0000

    upload completed twttr.py

---
+++ 2-Loops/twttr/twttr.py
@@ -0,0 +1,16 @@
+def remove_vowels(tweet):
+    vowels = "aeiouAeiou"
+    without_vowels = ""
+    for char in tweet:
+        if char not in vowels:
+            without_vowels += char
+    return without_vowels
+
+
+def tweet():
+    tweet = input("write a tweet, ")
+    result = remove_vowels(tweet)
+    print(result)
+
+
+tweet()
